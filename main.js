992df2d7a2b656112b7f0bf742e3398ef922c6ae
nalisherovvna <mushknasiba@gmail.com>
2024-03-24 15:54:41 +0500

    it was my homework

---
+++ main.js
@@ -0,0 +1,20 @@
+// const array = [1,2,3,4,5];
+// const array1 =["Madina,Husniya,Nasiba,Muhammadjon,Islomhoja"];
+
+
+// for (const element of array) {
+//     console.log(element);
+//     console.log(array1);
+// }
+
+
+const array = ["non" ,"aziza"];
+const array2 = ["mashina", "gul"];
+
+// for (const element of array) {
+//     array2.push(element);
+
+// }
+
+console.log(array);
+console.log(array2);
